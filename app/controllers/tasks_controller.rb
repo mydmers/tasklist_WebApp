d09e01e579d6fb53ab9e93fd966b1a4c4271f042
yosi <dreammakerscoltd@gmail.com>
2017-11-19 15:40:11 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,59 +1,34 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
 
-    
-    def index
-        @tasks = Task.order(created_at: :desc).page(params[:page]).per(25)
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
     end
+  end
 
-    def show
-    end
-
-    def new
-        @task = Task.new
-    end
-
-    def create
-        @task = Task.new(task_params) 
-        if @task.save 
-        flash[:success] = 'task が正常に投稿されました' 
-        redirect_to @task 
-        else
-        flash.now[:danger] = 'task が投稿されませんでした' 
-        render :new 
-        end
-    end
-
-    def edit
-    end
-
-    def update
-        if @task.update(task_params) 
-        flash[:success] = 'task は正常に更新されました' 
-        redirect_to @task 
-        else
-        flash.now[:danger] = 'task は更新されませんでした' 
-        render :edit 
-        end
-    end
-
-    def destroy
+  def destroy
     @task.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
 
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+  private
+
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
-
-
-    private
-
-    def set_task
-        @task = Task.find(params[:id])
-    end
-
-    # Strong Parameter
-    def task_params
-        params.require(:task).permit(:content, :status)
-    end
-    
+  end
 end
